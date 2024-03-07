eb0b344469a73b79446c1fa4bfa47fcbccc8a025
avilapro <mejiasavilaeduardo@gmail.com>
2024-03-07 17:23:07 -0400

    clase 2 - two way binding, manejadores de eventos y condicionales

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
 import { reactive, ref } from "vue";
-
+//Atributos del componente (ref, reactive)
 const inicio = reactive({
-  name: 'Inicio'
+  btn1: 'Inicio',
+  btn2: 'Series',
+  btn3: 'Peliculas',
+  btn4: 'Novedades Populares',
+  btn5: 'Mi Lista',
+  btn6: 'Explora por idiomas'
 })
 const series = ref('Series');
 const varPesoFuente = ref('textoNegrita');
@@ -17,30 +22,93 @@ let cssLiSubrayado = reactive({
     }
   }
 })
+
+let banner = reactive({
+  title: 'ONE PIECE',
+  subTitle: 'Adventure will continue'
+})
+let colorTxtBanner = ref('colorTxtBanner');
+
+//atributos del formulario de login
+//input text: nombre
+let nombreRegistro = ref('');
+//input radio: sexo
+let male = ref('Masculino');
+let female = ref('Femenino');
+let sexoSeleccionado = ref('Masculino');
+//input checkbox: preferencias
+let prefers = ref([])
+
+/**
+ * //Metodos del componente
+ */
+function mostrarInfo(info) {
+  alert(`Diste click en el boton ${info}`);
+}
+function mostrarEvent(event) {
+  console.log(event);
+  alert( "Diste click en: " +event.target.innerHTML);s
+}
 </script>
 
 <template>
-  <header>
+  <header @click.self="mostrarInfo('Click en el Header')">
     <img style="width: 150px; height: 70%;" v-bind:src="urlLogo.url" alt="">
     <nav id="navHeader">
       <ul id="linksNav">
-        <li :class="varPesoFuente">{{ inicio.name }}</li>
-        <li v-bind="cssLiSubrayado.st">{{ series }}</li>
-        <li>Peliculas</li>
-        <li>Novedades populares</li>
-        <li>Mi lista</li>
-        <li>Explora por idiomas</li>
+        <li :class="varPesoFuente" v-on:click.stop="mostrarEvent">{{ inicio.btn1 }}</li>
+        <li v-bind="cssLiSubrayado.st" v-on:click.stop="mostrarInfo(inicio.btn2)">{{ inicio.btn2 }}</li>
+        <li v-on:click.stop="mostrarInfo(inicio.btn3)">{{ inicio.btn3 }}</li>
+        <li @click.stop="mostrarInfo(inicio.btn4)">{{ inicio.btn4 }}</li>
+        <li @click.stop="mostrarInfo(inicio.btn5)">{{ inicio.btn5 }}</li>
+        <li @click.stop="mostrarInfo(inicio.btn6)">{{ inicio.btn6 }}</li>
       </ul>
     </nav>
   </header>
+  <main>
+    <section id="banner">
+      <h1 class="tituloBanner" :class="colorTxtBanner"> {{ banner.title }} </h1>
+      <p class="parrafoBanner" :class="colorTxtBanner"> {{ banner.subTitle }} </p>
+      <!-- Botones de la seccion Banner -->
+    </section>
+    <section id="registerForm" style="padding: 15vh; display: flex;">
+      <form style="border: solid 1px black; border-radius: 12px; width: fit-content; padding: 25px;" action="">
+        <h1>Registro</h1>
+        <p>Nombre escrito: {{ nombreRegistro }}</p>
+        Nombre: <input type="text" name="" id="" v-model="nombreRegistro" maxlength="10"><br><br>
+        Sexo: 
+        <label for="male">Masculino</label>
+        <input type="radio" name="sexo" :value="male" v-model="sexoSeleccionado" id="male">
+        <label for="female">Femenino</label>
+        <input type="radio" name="sexo" :value="female" v-model="sexoSeleccionado" id="female"><br><br>
+        <p>Preferencias seleccionadas: <span v-if="prefers.length > 0">{{ prefers }}</span> </p>
+        Preferencias:
+        <input type="checkbox" name="a" id="pa" value="Arte" v-model="prefers">
+        <label for="pa">Arte</label>
+        <input type="checkbox" name="c" id="pc" value="Ciencia" v-model="prefers">
+        <label for="pc">Ciencia</label><br><br>
+      </form>
+      <div style="display: flex; flex-direction: column;">
+        <div id="malex" v-if="sexoSeleccionado === male">Masculino</div>
+        <template v-if="sexoSeleccionado === female">
+          <p>Femenino</p>
+          <p>Mas complicado</p>
+        </template>
+        <div id="science" v-if="prefers.includes('Ciencia')">Ciencia</div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
 header {
-  background-image: linear-gradient(to bottom, #5a5e62, #818387d2, #aaabadb9, #d4d4d59f, #ffffff81);
+  background-image: linear-gradient(to bottom, #ffffff30, #ebebeb47, #d7d7d74a, #c4c4c441, #b1b1b11a);
   height: 70px;
   width: 100vw;
   padding: 0 2%;
+  position: fixed;
+  top: 0;
+  z-index: 100;
 }
 #navHeader{
   width: 60vw;
@@ -58,6 +126,30 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+/**Estilos del main */
+
+
+/**Estilos del banner */
+#banner{
+  background-image: url("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiXxN_42vXeZdwTL8NitxpeHe3MpgTKOKYAOShXZpFVw3E7T9MFc-7kRZsPrv8KgUgBLZPxQeXreDjnhRka9Qqpt5eZYSvcRlnh0HHp2Std2PwyxNFETDbPGII2o5vEp11tg5UbFxdylhjIwFKGv398jIdqp4LitvgYPTwiHgJNEAM1Ir2laveB6tVeNw/w640-h360/one-piece-1675092909888.jpg");
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  background-position: center center;
+  height: 100vh;
+  position: relative;
+  top: 0;
+  z-index: -5;
+  width: 98.7vw;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: space-around;s */
+  padding: 20vh 3vh;
+}
+.colorTxtBanner{
+  color: #ffffff !important;
+  font-size: 3.5em;
 }
 
 @media (min-width: 1024px) {
